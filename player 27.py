81e6eabe2af35e5c8a6787534eb8569880403835
Mohanakrishnan1199 <51222571+Mohanakrishnan1199@users.noreply.github.com>
2019-07-26 16:07:08 +0530

    Update player 27.py

--- player 27.py
+++ player 27.py
@@ -2,8 +2,8 @@ m=input()
 for i in m:
     if i.islower():
         a=i.upper()
-        print(a,end=" ")
+        print(a,end="")
     else:
         a=i.lower()
-        print(a,end=" ")
+        print(a,end="")
         
